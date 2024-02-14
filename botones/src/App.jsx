cda0f3040ce5a38e5462a0140d8afee36d85cfaf
Ian Levi Lagunes Gallegos <ilgallegos.f@gmail.com>
2024-02-14 10:58:33 -0600

    'Botones'

---
+++ botones/src/App.jsx
@@ -0,0 +1,33 @@
+// hooks
+import { useState } from "react"
+let contador = 0
+
+
+function App() {
+  const [contador, setContador]=useState(0)
+
+  const handleBotonIncrementar = ()=>{
+  setContador(contador + 1)
+}
+
+const handleBotonReinicio = ()=>{
+  setContador(contador - contador)
+}
+
+const handleBotonDecrementar = ()=>{
+  setContador(contador - 1)
+}
+  return (
+    <div>
+      <h1>CONTADOR</h1>
+      <hr/>
+      <h2>{contador}</h2>
+      <button onClick={handleBotonIncrementar}>Incrementar</button>
+      <button onClick={handleBotonReinicio}>Reinicio</button>
+      <button onClick={handleBotonDecrementar}>Decrementar</button>
+    </div>
+
+  )
+}
+
+export default App
